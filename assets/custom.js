9a9e7b5832a378334e922280c2281ccf055d3299
sdragon0105 <77851434+sdragon0105@users.noreply.github.com>
2021-07-02 05:28:24 +0300

    New create project

---
+++ assets/custom.js
@@ -0,0 +1,12 @@
+
+let content_item = document.querySelectorAll('.image-content');
+for (let i = 0; i < content_item.length; i++) {
+    let width1 = content_item[i].getBoundingClientRect().width;
+    let width2 = content_item[i].querySelector('.image-info').getBoundingClientRect().width;
+
+    let width3 = width1 - width2;
+    width3 = width3 / 2;
+    console.log(width3);
+
+    content_item[i].querySelector('.group-grid-product__meta--position').style.left = width3+"px";
+}
